26cefe968d480053804bb1558d1db8c598891386
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-20 19:47:54 +0530

    Add basic login component

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Categories from './pages/Categories';
-
+import LoginPage from './pages/login/Login';
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <HomePage />,
     },
     {
-        path: '/categories',
-        element: <Categories />,
+        path: '/auth/login',
+        element: <LoginPage />,
     },
 ]);
