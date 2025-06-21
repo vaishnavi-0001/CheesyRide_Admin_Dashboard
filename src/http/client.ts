9742b730239582b65c3d61e9193ca6d3cded9b5e
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-21 17:04:12 +0530

    Extend refresh token Validity

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import { useAuthStore } from '../store';
 
-console.log('🔥 BACKEND URL:', import.meta.env.VITE_BACKEND_API_URL);
 export const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_URL,
     withCredentials: true,
@@ -9,3 +9,35 @@ export const api = axios.create({
         Accept: 'application/json',
     },
 });
+
+const refreshToken = async () => {
+    await axios.post(
+        `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+        {},
+        {
+            withCredentials: true,
+        }
+    );
+};
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response.status === 401 && !originalRequest._isRetry) {
+            try {
+                originalRequest._isRetry = true;
+                const headers = { ...originalRequest.headers };
+                await refreshToken();
+                return api.request({ ...originalRequest, headers });
+            } catch (err) {
+                console.error('Token refresh error', err);
+                useAuthStore.getState().logout();
+                return Promise.reject(err);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import { self } from '../http/api';
 import { useAuthStore } from '../store';
+import { AxiosError } from 'axios';
 
 const getSelf = async () => {
     const { data } = await self();
@@ -15,6 +16,12 @@ const Root = () => {
     const { data, isLoading } = useQuery({
         queryKey: ['self'],
         queryFn: getSelf,
+        retry: (failureCount: number, error) => {
+            if (error instanceof AxiosError && error.response?.status === 401) {
+                return false;
+            }
+            return failureCount < 3;
+        },
     });
 
     useEffect(() => {
