8c9b5df90848316a55303d69e4238a042282e10e
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-22 00:54:12 +0530

    Integrate pagination

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -8,6 +8,7 @@ import { useAuthStore } from '../../store';
 import UsersFilter from './UsersFilter';
 import React from 'react';
 import UserForm from './forms/UserForm';
+import { PER_PAGE } from '../../../constants';
 
 const columns = [
     {
@@ -46,6 +47,12 @@ const Users = () => {
         token: { colorBgLayout },
     } = theme.useToken();
 
+    const [queryParams, setQueryParams] = React.useState({
+        perPage: PER_PAGE,
+        currentPage: 1,
+    });
+
+
     const [drawerOpen, setDrawerOpen] = React.useState(false);
     const {
         data: users,
@@ -53,9 +60,12 @@ const Users = () => {
         isError,
         error,
     } = useQuery({
-        queryKey: ['users'],
+        queryKey: ['users', queryParams],
         queryFn: () => {
-            return getUsers().then((res) => res.data);
+            const queryString = new URLSearchParams(
+                queryParams as unknown as Record<string, string>
+            ).toString();
+            return getUsers(queryString).then((res) => res.data);
         },
     });
     
@@ -105,7 +115,25 @@ const Users = () => {
                 </UsersFilter>
                    
 
-<Table columns={columns} dataSource={users} rowKey={'id'} />
+                <Table
+                    columns={columns}
+                    dataSource={users?.data}
+                    rowKey={'id'}
+                    pagination={{
+                        total: users?.total,
+                        pageSize: queryParams.perPage,
+                        current: queryParams.currentPage,
+                        onChange: (page) => {
+                            console.log(page);
+                            setQueryParams((prev) => {
+                                return {
+                                    ...prev,
+                                    currentPage: page,
+                                };
+                            });
+                        },
+                    }}
+                />
 <Drawer
                     title="Create user"
                     width={720}
