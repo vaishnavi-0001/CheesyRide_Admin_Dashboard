cd0085e21487c66a826930719aa394934fd53af4
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-21 21:12:32 +0530

    Homepage And User page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,41 @@
+import { Breadcrumb } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../../http/api';
+import type { User } from '../../types';
+
 const Users = () => {
-    return <div>Users</div>;
+    const {
+        data: users,
+        isLoading,
+        isError,
+        error,
+    } = useQuery({
+        queryKey: ['users'],
+        queryFn: () => {
+            return getUsers().then((res) => res.data);
+        },
+    });
+    return (
+        <>
+            <Breadcrumb
+                separator={<RightOutlined />}
+                items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
+            />
+            {isLoading && <div>Loading...</div>}
+            {isError && <div>{error.message}</div>}
+            {users && (
+                <div>
+                    <ul>
+                        {users.map((user: User) => (
+                            <li key={user.id}>{user.firstName}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Users;
