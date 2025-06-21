9b4ebe530d0746b5a64512be625d2d411cb926c1
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-21 13:19:21 +0530

    logout with mutation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,8 +3,9 @@ import { LockFilled, UserOutlined, LockOutlined } from '@ant-design/icons';
 import Logo from '../../components/icons/Logo';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import type { Credentials } from '../../types';
-import { login, self } from '../../http/api';
+import { login, self, logout } from '../../http/api';
 import { useAuthStore } from '../../store';
+import { usePermission } from '../../hooks/usePermissions';
 
 const loginUser = async (credentials: Credentials) => {
     // server call logic
@@ -18,7 +19,8 @@ const getSelf = async () => {
 };
 
 const LoginPage = () => {
-    const { setUser } = useAuthStore();
+    const { isAllowed } = usePermission();
+    const { setUser, logout: logoutFromStore } = useAuthStore();
 
     const { refetch } = useQuery({
         queryKey: ['self'],
@@ -26,11 +28,29 @@ const LoginPage = () => {
         enabled: false,
     });
 
+    const { mutate: logoutMutate } = useMutation({
+        mutationKey: ['logout'],
+        mutationFn: logout,
+        onSuccess: async () => {
+            logoutFromStore();
+            return;
+        },
+    });
+
     const { mutate, isPending, isError, error } = useMutation({
         mutationKey: ['login'],
         mutationFn: loginUser,
         onSuccess: async () => {
              const selfDataPromise = await refetch();
+              // logout or redirect to client ui
+            // window.location.href = "http://clientui/url"
+            // "admin", "manager", "customer"
+            if (!isAllowed(selfDataPromise.data)) {
+                await logout();
+                logoutFromStore();
+                logoutMutate();
+                return;
+            }
             setUser(selfDataPromise.data);
         },
     });
