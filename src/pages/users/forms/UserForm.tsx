d87330362acfd1c8619f1ffadcbb188b40e3d9cb
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-23 00:04:36 +0530

    Hide restaurant section for admin

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import type { Tenant } from '../../../types';
 
 const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
+    const selectedRole = Form.useWatch('role');
     const { data: tenants } = useQuery({
         queryKey: ['tenants'],
         queryFn: () => {
@@ -84,7 +85,7 @@ const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
                         </Card>
                     )}
 
-                    <Card title="Role" bordered={false}>
+                    <Card title="Role">
                         <Row gutter={20}>
                             <Col span={12}>
                             <Form.Item
@@ -105,34 +106,36 @@ const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
                                         placeholder="Select role">
                                         <Select.Option value="admin">Admin</Select.Option>
                                         <Select.Option value="manager">Manager</Select.Option>
-                                        <Select.Option value="customer">Customer</Select.Option>
+                                        
                                     </Select>
                                 </Form.Item>
                             </Col>
-                            <Col span={12}>
-                            <Form.Item
-                                    label="Restaurant"
-                                    name="tenantId"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Restaurant is required',
-                                        },
-                                    ]}>
-                                    <Select
-                                        size="large"
-                                        style={{ width: '100%' }}
-                                        allowClear={true}
-                                        onChange={() => {}}
-                                        placeholder="Select restaurant">
-                                        {tenants?.data.map((tenant: Tenant) => (
-                                             <Select.Option value={tenant.id} key={tenant.id}>
-                                                {tenant.name}
-                                            </Select.Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                            </Col>
+                            {selectedRole === 'manager' && (
+                                <Col span={12}>
+                                    <Form.Item
+                                        label="Restaurant"
+                                        name="tenantId"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Restaurant is required',
+                                            },
+                                        ]}>
+                                        <Select
+                                            size="large"
+                                            style={{ width: '100%' }}
+                                            allowClear={true}
+                                            onChange={() => {}}
+                                            placeholder="Select restaurant">
+                                            {tenants?.data.map((tenant: Tenant) => (
+                                                <Select.Option value={tenant.id} key={tenant.id}>
+                                                    {tenant.name}
+                                                </Select.Option>
+                                            ))}
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                            )}
                         </Row>
                     </Card>
                 </Space>
